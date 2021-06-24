ce3d4d1aa1ccaf6ab5c5d71e48c587e524c2b817
simonchen92 <simon.k.chen92@gmail.com>
2021-06-24 15:04:53 -0400

    feat(forms): created model & form submission functionality

--- src/app/add-item-form/add-item-form.component.ts
+++ src/app/add-item-form/add-item-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Input, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { BudgetItem } from '../shared/models/budget-item.model';
 
 @Component({
   selector: 'app-add-item-form',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddItemFormComponent implements OnInit {
 
+  @Input() item: BudgetItem = new BudgetItem('', 0);
+  @Output() formSubmit: EventEmitter<BudgetItem> = new EventEmitter<BudgetItem>()
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(form: NgForm) {
+    this.formSubmit.emit(form.value)
   }
 
 }
