c8afdc3131242f1e3dc8226a5722b9c25c3bbb13
simonchen92 <simon.k.chen92@gmail.com>
2021-06-25 13:24:53 -0400

    feat(forms): create functionality to delete items and form reset

--- src/app/budget-item-list/budget-item-card/budget-item-card.component.ts
+++ src/app/budget-item-list/budget-item-card/budget-item-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { BudgetItem } from 'src/app/shared/models/budget-item.model';
 
 @Component({
@@ -9,10 +9,15 @@ import { BudgetItem } from 'src/app/shared/models/budget-item.model';
 export class BudgetItemCardComponent implements OnInit {
 
   @Input() item: BudgetItem = new BudgetItem('', 0);
+  @Output() xButtonClick: EventEmitter<any> = new EventEmitter<any>()
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onXButtonClick() {
+    this.xButtonClick.emit()
   }
 
 }
